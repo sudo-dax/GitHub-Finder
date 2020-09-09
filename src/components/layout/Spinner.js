37dab439fe33ba9452ea2a44f8f7a5c8e06f9c5a
sudo-dax <dax@scholtz.com.au>
2020-09-09 11:44:43 +1000

    first commit - code base

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,13 @@
+import React, {fragment} from 'react'
+import spinner from './spinner.gif'
+import { Fragment } from 'react'
+
+const Spinner = () => {
+    return (
+        <Fragment>
+            <img src={spinner} alt="Loading..." style={{ width: '200px', margin: 'auto', display: 'block '}} />        
+        </Fragment>
+    )
+}
+
+export default Spinner
